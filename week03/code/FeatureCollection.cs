0ca696286357438d92744f0aa86be752072fe645
gumboguru01 <aslinfomationdesk@gmail.com>
2025-07-22 20:17:46 +0300

    updatd

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+
 public class FeatureCollection
 {
     public List<Feature> Features { get; set; }
@@ -13,30 +18,34 @@ public class Properties
     public double? Mag { get; set; }
     public string Place { get; set; }
 }
-public static string[] EarthquakeDailySummary()
+
+public static class EarthquakeHelper
 {
-    const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-
-    using var client = new HttpClient();
-    using var request = new HttpRequestMessage(HttpMethod.Get, uri);
-    using var response = client.Send(request);
-    using var stream = response.Content.ReadAsStream();
-
-    var options = new JsonSerializerOptions
+    public static string[] EarthquakeDailySummary()
     {
-        PropertyNameCaseInsensitive = true
-    };
+        const string uri = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-    var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(stream, options);
+        using var client = new HttpClient();
+        using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+        using var response = client.Send(request);
+        using var stream = response.Content.ReadAsStream();
 
-    var result = new List<string>();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
-    foreach (var feature in featureCollection.Features)
-    {
-        string place = feature.Properties.Place ?? "Unknown place";
-        string mag = feature.Properties.Mag.HasValue ? feature.Properties.Mag.Value.ToString("0.00") : "N/A";
-        result.Add($"{place} - Mag {mag}");
+        var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(stream, options);
+
+        var result = new List<string>();
+
+        foreach (var feature in featureCollection.Features)
+        {
+            string place = feature.Properties.Place ?? "Unknown place";
+            string mag = feature.Properties.Mag.HasValue ? feature.Properties.Mag.Value.ToString("0.00") : "N/A";
+            result.Add($"{place} - Mag {mag}");
+        }
+
+        return result.ToArray();
     }
-
-    return result.ToArray();
 }
